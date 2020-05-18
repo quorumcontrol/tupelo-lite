70ba2541e6c111dcbdcbb152f3996a308b3cd8ab
Topper Bowers <topper@toppingdesign.com>
2020-05-18 20:04:32 +0200

    continued release boilerplate

--- scripts/_docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# This file should be executed INSIDE of docker
-
-set -x
-
-cd /app/tupelo/aggregator/wasm
-GOOS=js GOARCH=wasm go build -gcflags=-trimpath="${PWD}" -asmflags=-trimpath="${PWD}" -ldflags='-s -w' -o /app/src/js/go/tupelo.wasm
